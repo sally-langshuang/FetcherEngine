237b65f52377f731a3ef0c285719e51509f69f4b
langshuang <langshuang01@baidu.com>
2021-07-23 15:42:04 +0800

    h

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main()  {
-	fmt.Println("hello world")
+	resp, err := http.Get("https://www.baidu.com/robots.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		all, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", all)
+	}
 }
