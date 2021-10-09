c411ac4898d3b3f31435db2f6ef261aba9f273c2
sally-langshuang <1009523335@qq.com>
2021-10-09 18:55:22 +0800

    1009

--- leet/graph_basic.go
+++ leet/graph_basic.go
@@ -1,5 +1,7 @@
 package leet
 
+import "fmt"
+
 type Node struct {
 	ID int
 }
@@ -28,30 +30,46 @@ type G struct {
 	}
 }
 
-const (
-	Less        = 1 << iota
-	Equal
-	Larger
-	LessEqual   = Less & Equal
-	LargerEqual = Larger & Equal
-)
 
 type Element interface {
 }
 
-type Key interface {
-}
-
-type CompareElement interface {
-	Compare(e CompareElement) byte
-}
 
 type Set interface{
-	Search(s Set, key Key) *Element
+	Search(s Set, key int) *Element
 	Insert(s Set, x *Element)
 	Delete(s Set, x *Element)
 	Minimum(s Set) *Element
 	Maximum(s Set) *Element
 	Successor(s Set, x *Element) *Element
 	Predecessor(s Set, x *Element) *Element
+}
+
+// 有限栈
+
+type Stack1 struct {
+	Arr []int
+	TopToPushIdx int
+}
+
+func (s Stack1) Empty() bool {
+	return s.TopToPushIdx == 0
+}
+
+func (s Stack1) Push(val int) error {
+	if s.TopToPushIdx == len(s.Arr) {
+		return fmt.Errorf("stack overflows")
+	}
+	s.Arr[s.TopToPushIdx] = val
+	s.TopToPushIdx += 1
+	return nil
+}
+
+func (s Stack1) Pop() (v int, e error){
+	if s.Empty() {
+		return -1, fmt.Errorf("no element to pop")
+	}
+	v = s.Arr[s.TopToPushIdx - 1]
+	s.TopToPushIdx -= 1
+	return
 }
