5f9fb58445947df30867d74007ee065a984d8ed5
sally-langshuang <1009523335@qq.com>
2021-11-01 17:36:41 +0800

    1101

--- leet/num8.go
+++ leet/num8.go
@@ -1,5 +1,38 @@
 package leet
 
+
 func myAtoi(s string) (res int) {
-	return
+	const (
+		stripLStep =  1 << iota
+		sginStep
+		readStep
+	)
+
+	step := stripLStep
+	sign := 1
+	for i:= range s {
+		switch step {
+		case stripLStep:
+			if i == ' ' {
+				continue
+			} else {
+				step = step << 1
+			}
+			fallthrough
+		case sginStep:
+			if i == '-' {
+				sign = -1
+			} else {
+				step = step << 1
+			}
+			fallthrough
+		case readStep:
+			if i > '0' || i < '9' {
+				res = res*10 + i - '0'
+			} else {
+				return res * sign
+			}
+		}
+	}
+	return res * sign
 }
