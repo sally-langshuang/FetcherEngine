74043d1ca6765ae00cc0571137354a64d5fcc06c
sally-langshuang <1009523335@qq.com>
2021-11-10 12:10:46 +0800

    Palindrome

--- leet/tree/num6.go
+++ leet/tree/num6.go
@@ -1,4 +1,18 @@
 package tree
-func isPalindrome(x int) bool {
 
+import "strconv"
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	s := strconv.Itoa(x)
+	for i, j := 0, len(s) - 1; i <= j; {
+		if (s[i] != s[j]) {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
 }
