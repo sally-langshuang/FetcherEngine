804d58c99a827f0acee273f6753dc31f34dcc563
sally-langshuang <1009523335@qq.com>
2021-12-14 15:41:22 +0800

    a

--- daily/dp/num630.go
+++ daily/dp/num630.go
@@ -1,40 +1,10 @@
 package dp
 
-import "fmt"
-
-func max(a, b int) int{
-	if a >= b {
-		return a
-	}
-	return b
-}
-
-func dp(courses [][]int, i, offset int, f*[]map[int]int) int {
-	if i >= len(courses) {
-		return 0
-	}
-	if v, ok := (*f)[i][offset]; ok {
-		return v
-	}
-	if len(courses) == i + 1 {
-		if courses[i][1] >= offset + courses[i][0] {
-			(*f)[i][offset] = 1
-			return 1
-		} else {
-			(*f)[i][offset] = 0
-			return 0
-		}
-	}
-	yes := 1 + dp(courses, i + 1, offset + courses[i][0], f)
-	no := dp(courses, i + 1, offset, f)
-	if courses[i][1] >= offset + courses[i][0] {
-		(*f)[i][offset] = max(yes, no)
-		return (*f)[i][offset]
-	}
-	(*f)[i][offset] = no
-	return (*f)[i][offset]
-}
-
+import (
+	"container/heap"
+	"fmt"
+	"sort"
+)
 func isAsc(a, b[]int) bool {
 	if a[1] < b[1] {
 		return true
@@ -44,7 +14,8 @@ func isAsc(a, b[]int) bool {
 	}
 	return false
 }
-func sort(courses [][]int)  {
+
+func sort1(courses [][]int)  {
 	if len(courses) <= 1 {
 		return
 	}
@@ -66,18 +37,117 @@ func sort(courses [][]int)  {
 		courses[i], courses[j] = courses[j], courses[i]
 	}
 	out:
-	sort(courses[:i])
-	sort(courses[i + 1:])
+	sort1(courses[:i])
+	sort1(courses[i + 1:])
 	return
+}
+type arr []int
+
+type MyHeap struct {
+	arr
+}
+
+func (h *MyHeap) up(i int)  {
+	for x := i; x > 0; {
+		up := (x - 1) / 2
+		if h.arr[up] < h.arr[x] {
+			h.arr[up], h.arr[x] = h.arr[x], h.arr[up]
+		} else {
+			break
+		}
+		x = up
+	}
+}
+
+func (h *MyHeap) Push(v int)  {
+	h.arr = append(h.arr, v)
+	h.up(len(h.arr) - 1)
+}
+
+func (h *MyHeap) down(i0 int) bool {
+	i := i0
+	for {
+		j := i * 2 + 1
+		if j >= len(h.arr) {
+			break
+		}
+		if j2 := j + 1; j2 < len(h.arr) && h.arr[j2] > h.arr[j] {
+			j = j2
+		}
+		if h.arr[j] <= h.arr[i] {
+			break
+		}
+		h.arr[j], h.arr[i] = h.arr[i], h.arr[j]
+		i = j
+	}
+	return i > i0
 
 }
-func scheduleCourse(courses [][]int) int {
-	sort(courses)
-	f := make([]map[int]int, len(courses))
-	for i:= range  f {
-		f[i] = map[int]int{}
+func (h *MyHeap) Fix(i int)  {
+	if !h.down(i) {
+		h.up(i)
 	}
-	res :=  dp(courses, 0, 0, &f)
-	fmt.Println(f)
-	return  res
 }
+
+func scheduleCourse0(courses [][]int) int {
+	sort1(courses)
+	var offset, maxCount int
+	h := MyHeap{arr{}}
+	for i := range courses {
+		if courses[i][0] + offset <= courses[i][1] {
+			offset += courses[i][0]
+			h.Push(courses[i][0])
+			maxCount++
+		} else if len(h.arr) > 0 {
+			if h.arr[0] > courses[i][0] {
+				offset += - h.arr[0] + courses[i][0]
+				h.arr[0] = courses[i][0]
+				h.Fix(0)
+			}
+		}
+	}
+	return len(h.arr)
+}
+
+func scheduleCourse(courses [][]int) int {
+	sort.Slice(courses, func(i, j int) bool {
+		return courses[i][1] < courses[j][1]
+	})
+
+	h := &Heap{}
+	total := 0 // 优先队列中所有课程的总时间
+	for _, course := range courses {
+		if h.Len() == 191 {
+			fmt.Println("debug")
+		}
+		if t := course[0]; total+t <= course[1] {
+			total += t
+			heap.Push(h, t)
+		} else if h.Len() > 0 && t < h.IntSlice[0] {
+			total += t - h.IntSlice[0]
+			h.IntSlice[0] = t
+			heap.Fix(h, 0)
+		}
+	}
+	return h.Len()
+}
+
+type Heap struct {
+	sort.IntSlice
+}
+
+func (h Heap) Less(i, j int) bool {
+	return h.IntSlice[i] > h.IntSlice[j]
+}
+
+func (h *Heap) Push(x interface{}) {
+	h.IntSlice = append(h.IntSlice, x.(int))
+}
+
+func (h *Heap) Pop() interface{} {
+	a := h.IntSlice
+	x := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return x
+}
+
